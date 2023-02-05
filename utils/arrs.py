d0a90bf943f390a24e66327d2e2957122c399520
anastasiya <burykh_2000@mail.ru>
2023-02-05 14:49:15 +0300

    добавлены условия

--- utils/arrs.py
+++ utils/arrs.py
@@ -14,7 +14,10 @@ def get(array, index, default=None):
     if index < 0:
         return default
 
-    return array[index]
+    elif index not in array:
+        return default
+
+    return array[-index]
 
 
 def my_slice(coll, start=0, end=None):
